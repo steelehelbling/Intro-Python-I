fe1d2c0bb0d96be4975eb580703673ffaf9bf9ac
Steele Helbling <steelehelbling@gmail.com>
2020-08-05 16:10:34 -0400

    ver-3

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -4,13 +4,14 @@
 # When you use a variable in a function, it's local in scope to the function.
 x = 12
 
-def change_x(y):
-  return 99
+def change_x():
+    global x
+    x = 99
 
-val = change_x(x)
+change_x()
 
 # This prints 12. What do we have to modify in change_x() to get it to print 99?
-print(val)
+print(x)
 
 
 # This nested function has a similar problem.
@@ -18,14 +19,15 @@ print(val)
 def outer():
     y = 120
 
-    def inner(x):
-        print (x+y)
+    def inner():
+        nonlocal y
+        y = 999
 
-    
+    inner()
 
     # This prints 120. What do we have to change in inner() to get it to print
     # 999?
     # Note: Google "python nested function scope".
-    print(inner(879))
+    print(y)
 
 outer()
